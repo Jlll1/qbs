607462036a1a8d6fe7b27c0b05fc21cb6fdecf5d
Jlll1 <arghantentua@tutanota.com>
2022-02-04 15:55:04 +0100

    Initial script setup

--- setup.sh
+++ setup.sh
@@ -1,24 +1,45 @@
 #!/bin/sh
 
-VOID_MKLIVE='https://github.com/void-linux/void-mklive/archive/master.tar.gz'
-VOID_PACKAGES='https://github.com/void-linux/void-packages/archive/master.tar.gz'
+VOID_MKLIVE="https://github.com/void-linux/void-mklive/archive/master.tar.gz"
+VOID_PACKAGES="https://github.com/void-linux/void-packages/archive/master.tar.gz"
+#DOTFILES="https://github.com/Jlll1/dotfiles/archive/master.tar.gz"
 
-mkdir -pv setup
+BASE_PKGS="dialog cryptsetup lvm2 mdadm void-docs-browse grub-x86_64-efi"
+SRC_PKGS="dmenu dwm"
+PACKAGES="$BASE_PKGS $SRC_PKGS xorg alacritty git make curl qutebrowser"
+
+
+# main()
+
+mkdir setup
 cd setup
+
 curl -LO $VOID_MKLIVE
 tar -xvf master.tar.gz && rm master.tar.gz
 cd void-mklive-master && make && cd ..
+
 curl -LO $VOID_PACKAGES
 tar -xvf master.tar.gz && rm master.tar.gz
+
+#curl -LO $DOTFILES
+#tar -xvf master.tar.gz && rm master.tar.gz
+
+#cp -rf dotfiles-master/void-packages/* void-packages-master/srcpkgs/
+#rm -rf dotfiles-master/void-packages/
+
+#mkdir -p /dir/home/rb
+#cp -rf dotfiles-master/.[^.]* /dir/home/rb/
+#rm -rf dotfiles-master
+
 cd void-packages-master
 ./xbps-src binary-bootstrap
-./xbps-src -j6 pkg dialog
-./xbps-src -j6 pkg xorg
-./xbps-src -j6 pkg dmenu
-./xbps-src -j6 pkg dwm
-./xbps-src -j6 pkg void-installer
+for PKG in $PACKAGES
+do
+  ./xbps-src -j8 pkg $PKG
+done
 cd ..
-mkdir -p dir/home/rb/
-touch dir/home/rb/test
+
 cd void-mklive-master
-sudo ./mklive.sh -a x86_64-musl -r ../void-packages-master/hostdir/binpkgs -I ../dir -T bytomski-void
+sudo ./mklive.sh -a x86_64-musl -r ../void-packages-master/hostdir/binpkgs -p "$PACKAGES"
+mv *.iso ../../ && cd ../..
+sudo rm -rf setup
