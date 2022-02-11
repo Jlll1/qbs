679bb16c89147ff30455efdbb2feb55d9069b31c
Jlll1 <arghantentua@tutanota.com>
2022-02-11 14:06:14 +0100

    Add more packages, handle DOTFILES and change arch to x86_64

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+TARGET_ARCH=x86_64
+
 VOID_MKLIVE="https://github.com/void-linux/void-mklive/archive/master.tar.gz"
 VOID_PACKAGES="https://github.com/void-linux/void-packages/archive/master.tar.gz"
-#DOTFILES="https://github.com/Jlll1/dotfiles/archive/master.tar.gz"
+DOTFILES="https://github.com/Jlll1/dotfiles/archive/master.tar.gz"
 
 BASE_PKGS="dialog cryptsetup lvm2 mdadm void-docs-browse grub-x86_64-efi"
+ESSENTIAL_PKGS="pulseaudio pavucontrol xorg alacritty"
+MEDIA_PKGS="qutebrowser mpv"
+DEV_PKGS="make git neovim nodejs vscode go"
+MISC_PKGS="curl"
 SRC_PKGS="dmenu dwm"
-PACKAGES="$BASE_PKGS $SRC_PKGS xorg alacritty git make curl qutebrowser"
+PACKAGES="$BASE_PKGS $ESSENTIAL_PKGS $MEDIA_PKGS $DEV_PKGS $MISC_PKGS $SRC_PKGS"
 
 
 # main()
@@ -21,25 +27,25 @@ cd void-mklive-master && make && cd ..
 curl -LO $VOID_PACKAGES
 tar -xvf master.tar.gz && rm master.tar.gz
 
-#curl -LO $DOTFILES
-#tar -xvf master.tar.gz && rm master.tar.gz
+curl -LO $DOTFILES
+tar -xvf master.tar.gz && rm master.tar.gz
 
-#cp -rf dotfiles-master/void-packages/* void-packages-master/srcpkgs/
-#rm -rf dotfiles-master/void-packages/
+cp -rf dotfiles-master/void-packages/* void-packages-master/srcpkgs/
+rm -rf dotfiles-master/void-packages/
 
-#mkdir -p /dir/home/rb
-#cp -rf dotfiles-master/.[^.]* /dir/home/rb/
-#rm -rf dotfiles-master
+mkdir -p dir/home/rb
+cp -rf dotfiles-master/.[^.]* dir/home/rb/
+rm -rf dotfiles-master
 
 cd void-packages-master
 ./xbps-src binary-bootstrap
-for PKG in $PACKAGES
+for PKG in $SRC_PKGS
 do
-  ./xbps-src -j8 pkg $PKG
+  ./xbps-src -j8 -a $TARGET_ARCH pkg $PKG
 done
 cd ..
 
 cd void-mklive-master
-sudo ./mklive.sh -a x86_64-musl -r ../void-packages-master/hostdir/binpkgs -p "$PACKAGES"
+sudo ./mklive.sh -a $TARGET_ARCH -r ../void-packages-master/hostdir/binpkgs -p "$PACKAGES" -I ../dir
 mv *.iso ../../ && cd ../..
 sudo rm -rf setup
